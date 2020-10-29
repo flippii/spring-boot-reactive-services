2267679cd0ab3232a760d8d6b734c1bb117c9ce7
flippii <Knut.Genthe@gmx.de>
2020-10-29 20:44:04 +0100

    add keycloak api configuration

---
+++ user-service/src/main/java/com/spring/boot/example/keycloak/KeycloakProperties.java
@@ -0,0 +1,7 @@
+package com.spring.boot.example.keycloak;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "keycloak")
+public class KeycloakProperties {
+}
